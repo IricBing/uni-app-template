0ab5d52b13fe939457a6513b09311fcaacdbc2b7
Iric <iricbing@gmail.com>
2021-04-22 15:26:24 +0800

    调整结构

--- src/router/build.js
+++ src/router/build.js
@@ -4,7 +4,7 @@ const router = require('./index.js');
 const subapp1 = require('./modules/subapp1');
 const subapp2 = require('./modules/subapp2');
 
-const builder = (app, baseUrl, children) => {
+const builder = (app, baseUrl, children, root) => {
   const routeList = [];
   for (const route of children) {
     if (route.children) routeList.push(...builder(baseUrl + route.path + '/', route.children));
@@ -12,14 +12,24 @@ const builder = (app, baseUrl, children) => {
       const item = {
         path: baseUrl + route.path + '/index',
         name: app + '/' + route.name,
+        home: route.home,
         style: {
           navigationBarTitleText: route.title
         }
       };
-      Object.keys(route).forEach(
-        prop => !['path', 'name', 'title'].includes(prop) && (item[prop] = Object.assign(item[prop] || {}, route[prop]))
-      );
+      Object.keys(route).forEach(prop => !['path', 'name', 'title'].includes(prop) && (item[prop] = Object.assign(item[prop] || {}, route[prop])));
       routeList.push(item);
+    }
+  }
+
+  // 如果是递归调用根节点，表示此时的routeList已经是完整的页面列表了，将带有home标志的元素移动到数组最开始。
+  if (root) {
+    const homePageIndex = routeList.findIndex(route => route.home);
+    if (~homePageIndex) {
+      const homePage = routeList.find(route => route.home);
+      delete homePage.home;
+      routeList.splice(homePageIndex, 1);
+      routeList.unshift(homePage);
     }
   }
 
@@ -30,7 +40,7 @@ const builder = (app, baseUrl, children) => {
 const buildRouter = (app, route) => {
   const { baseUrl, children } = route;
 
-  return builder(app, baseUrl, children);
+  return builder(app, baseUrl, children, true);
 };
 
 // 构建 pages
@@ -53,5 +63,6 @@ writeFile(
   join(__dirname, '..', 'pages.json'),
   // 我这边是用两个空格来缩进 pages.json，如果喜欢制表符，第三个参数更换你为 \t 即可
   JSON.stringify(router, null, '  '),
+  /* eslint-disable-next-line */
   e => (e ? console.error(e) : console.log('pages.json 配置文件更新成功'))
 );
--- src/router/modules/app/user.js
+++ src/router/modules/app/user.js
@@ -5,6 +5,7 @@ module.exports = {
       path: 'register',
       title: '注册',
       name: 'register',
+      home: true,
       style: {
         navigationBarTextStyle: 'black'
       },
